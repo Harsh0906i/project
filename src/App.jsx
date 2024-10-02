23ea5f26561eda92f944d79791e9185a8a62dbef
Harshit <harshitsingharya24@gmail.com>
2024-10-02 12:37:01 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Headers from "./components/Headers"
+import Projects from "./pages/Projects"
+import Services from "./pages/Services"
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Headers/>
+        <Headers />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/services" element={<Services />} />
         </Routes>
       </BrowserRouter>
     </>
